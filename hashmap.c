18da3b823627c510e7735303ca3e91c8254fa87f
Your Name <you@example.com>
2025-04-17 21:42:12 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -54,9 +54,9 @@ void enlarge(HashMap * map) {
 HashMap * createMap(long capacity) {
     HashMap* aux = (HashMap *) malloc(sizeof(HashMap));
     if (aux == NULL) exit(1);
-    aux->buckets = (Pair **) malloc(map->capacity * sizeof(Pair *));
-    if (aux->buckets == NULL) exit(1);
     aux->capacity = capacity;
+    aux->buckets = (Pair **) malloc(aux->capacity * sizeof(Pair *));
+    if (aux->buckets == NULL) exit(1);
     aux->size = 0;
     aux->current = 0;
 
